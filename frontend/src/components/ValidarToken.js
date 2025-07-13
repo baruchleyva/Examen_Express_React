69f8e5c9bf6956ad96d4ea01867dbed5b7493c86
Baruch Ordaz <baruchleyva23@gmail.com>
2025-07-12 22:01:10 -0600

    Validar token para las rutas del dashboard

---
+++ frontend/src/components/ValidarToken.js
@@ -0,0 +1,9 @@
+import { Navigate } from "react-router-dom";
+
+function ValidarToken({children}) {
+    const token = localStorage.getItem('token');
+    if(!token) return <Navigate to="/" replace />;
+    return children;
+}
+
+export default ValidarToken;
